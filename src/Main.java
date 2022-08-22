6af75866f4d4b4fc3b4f59cb0672e93779defb61
Ivan <jazzman.ain@gmail.com>
2022-08-22 22:42:14 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("ТАБЛИЦА УМНОЖЕНИЯ");
+        System.out.println();
+
+        for (int i = 1; i < 10; i++){
+            for (int j = 1; j < 10; j++){
+                System.out.println("%s x %s = %s".formatted(i,j,i*j));
+            }
+            System.out.println();
+        }
+    }
+}
