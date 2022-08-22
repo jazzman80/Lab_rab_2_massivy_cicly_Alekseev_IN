fb0ab42bcde0e76f09f7e37df0222791245ad8a5
Ivan <jazzman.ain@gmail.com>
2022-08-22 22:45:53 +0300

    Новое форматирование строки

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
         for (int i = 1; i < 10; i++){
             for (int j = 1; j < 10; j++){
-                System.out.println("%s x %s = %s".formatted(i,j,i*j));
+                System.out.printf("%s x %s = %s%n", i,j,i*j);
             }
             System.out.println();
         }
