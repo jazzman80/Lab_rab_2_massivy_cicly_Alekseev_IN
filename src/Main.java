12a512be7b16f230debca31128b774a2169f75b8
Ivan <jazzman.ain@gmail.com>
2022-08-22 22:55:11 +0300

    Классическая таблица умножения Как в тетрадке)

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,8 @@ public class Main {
         System.out.println("ТАБЛИЦА УМНОЖЕНИЯ");
         System.out.println();
 
-        for (int i = 1; i < 10; i++){
-            for (int j = 1; j < 10; j++){
+        for (int i = 2; i < 10; i++){
+            for (int j = 1; j < 11; j++){
                 System.out.printf("%s x %s = %s%n", i,j,i*j);
             }
             System.out.println();
